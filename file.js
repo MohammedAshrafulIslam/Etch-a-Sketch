135da160e4d2b1fa17befe3bf72e39396aa7bfe7
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2024-06-18 22:06:39 -0400

    added the code to generate random colors on every mouseovers

--- file.js
+++ file.js
@@ -3,17 +3,18 @@ const container = document.querySelector(".container");
 for(let i = 0; i < 256; i++){
  const box = document.createElement("div");
  box.classList.add("grid-squares");
- box.textContent = "I am here";
  container.appendChild(box);
 }
 
 let boxes = container.querySelectorAll(".grid-squares");
 boxes.forEach(el); 
 
+
 function el(e){
   e.addEventListener('mouseover', () => {
    // Change the button's background color
-   e.style.backgroundColor = 'blue';
+   e.style.backgroundColor = '#'+ Math.floor(Math.random()*16777215).toString(16); 
+   //generates random color and the # is added to make it rgb;
   });
 /*
   // Add a mouseout event listener
@@ -23,6 +24,7 @@ function el(e){
   });
 */
  }
+ console.log('#'+Math.floor(Math.random()*16777215).toString(16));
 
 
  const btn = document.createElement("button");
