2fdf12bd18d3ff83e20544c78cb029c448a8fb75
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2024-06-21 13:18:59 -0400

    wrote the code to create grid in the container without overflowing

--- file.js
+++ file.js
@@ -4,28 +4,38 @@ const gridBtn = document.createElement("button");
 gridBtn.classList.add("btn-grid");
 gridBtn.textContent = "Enter the size";
 buttons.appendChild(gridBtn);
-let gridSize = 16;
+let grids = 16;
 gridBtn.addEventListener('click', newGrid );
 
-function newGrid(){
-  newGridSize = Number(window.prompt("Type a number", ""));
-  gridSize = newGridSize;
-  createGrid();
-}
+
 
 //gridSize = Number(window.prompt("Type a number", ""));
 function createGrid(){
     container.innerHTML = ""; //clears the container before creating new grid, removes the grid from previous runs if any.
-    for(let i = 0; i < (gridSize*gridSize); i++){
-    const box = document.createElement("div");
-    box.classList.add("grid-squares");
-    container.appendChild(box);
+    if(grids === 100 || grids <= 0){
+      alert("Please enter a value between 1 and 100!")
+      newGrids = Number(window.prompt("Please set the sixe of the grid(maximum 100)", ""));
+    }
+    else{
+      for(let i = 0; i < (grids*grids); i++){
+      const box = document.createElement("div");
+      box.classList.add("grid-squares");
+      box.style.width = String(640/(grids)) + 'px';
+      box.style.height = String(640/(grids)) + 'px';
+      container.appendChild(box);
+      }
     }
   }
 createGrid();  
 let boxes = container.querySelectorAll(".grid-squares"); //selected all the grids using it parent element which is container.
 boxes.forEach(el); 
-
+function newGrid(){
+  newGrids = Number(window.prompt("Please set the sixe of the grid(maximum 100)", ""));
+  grids = newGrids;
+  createGrid();
+  let boxes = container.querySelectorAll(".grid-squares"); //selected all the grids using it parent element which is container.
+  boxes.forEach(el); 
+}
 
 function el(e){
   e.addEventListener('mouseover', () => {
@@ -33,13 +43,6 @@ function el(e){
    e.style.backgroundColor = '#'+ Math.floor(Math.random()*16777215).toString(16); 
    //generates random color and the # is added to make it rgb;
   });
-/*
-  // Add a mouseout event listener
-  e.addEventListener('mouseout', () => {
-   // Change the button's background color back to its original color
-   e.style.backgroundColor = '';
-  });
-*/
  }
  console.log('#'+Math.floor(Math.random()*16777215).toString(16));
 
